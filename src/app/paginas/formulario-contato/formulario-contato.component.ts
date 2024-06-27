58c962632b58f39aaa0d9a1ca2aac0c068eef9f6
Gabriel Leandro <gabriel.leandro@modalgr.com.br>
2024-06-27 17:46:02 -0300

    feat(curso2): modulo 5 concluido

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,31 +1,40 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
-  imports: [
-    ContainerComponent,
-    SeparadorComponent,
-    ReactiveFormsModule
-  ],
+  imports: [ContainerComponent, SeparadorComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './formulario-contato.component.html',
-  styleUrl: './formulario-contato.component.css'
+  styleUrl: './formulario-contato.component.css',
 })
 export class FormularioContatoComponent {
-
   contatoForm!: FormGroup;
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl(''),
-      telefone: new FormControl(''),
-      email: new FormControl(''),
-      nascimento: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      nascimento: new FormControl('', Validators.required),
       redes: new FormControl(''),
-      observacoes: new FormControl('')
-   })
+      observacoes: new FormControl(''),
+    });
   }
+
+  SalvarContato() {
+    
+    if (this.contatoForm.invalid) {
+      console.log("Formulário inválido");
+      return;
+    }
+    console.log(this.contatoForm.value);
+  }
+  Cancelar() {
+    console.log("Cancelado");
+  }
+  
 }
